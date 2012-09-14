d992ab9f9a8535301604c4d2e4390ccbe2820616
Alexander Conway <alexanderconway@Alexanders-MacBook-Pro-2.local>
2012-09-13 20:52:50 -0700

    -started api_app

---
+++ Resources/api/api_app.js
@@ -0,0 +1,65 @@
+var App; 
+
+
+var createHTTPClient = function(params) {
+	var xhr = Ti.Network.createHTTPClient();
+
+	xhr.timeout = 30000;
+
+	xhr.onerror = function(e) {
+		handleOnError(e, params);
+
+	};
+
+	xhr.onreadystatechange = function(e) {
+	};
+
+	xhr.onload = function(response) {
+
+		var responseType = "application/json";
+
+		if (response.source.getResponseHeader("Content-Type") && response.source.getResponseHeader("Content-Type").startsWith(responseType)) {
+			App.LOG("App.API xhr.onload Response [" + response.source.getResponseHeader("Content-Type") + "]");
+			params.callback(response, params);
+		} else {
+			App.LOG("App.API xhr.onload Content-Type: " + response.source.getResponseHeader("Content-Type"));
+			Ti.API.error(" * * * * Invalid response format " + response.source.responseText);
+			Ti.UI.createAlertDialog({
+				title : "Server Response Error",
+				message : "The server did not respond correctly.\nPerhaps you entered an invalid URL?" + "\nDebugging: (response not JSON)\n" + response.source.responseText
+			}).show();
+			App.UI.hideWait();
+		}
+		xhr = null;
+	};
+
+	return xhr;
+};
+
+exports.send = function(params) {
+
+	App.LOG("App.API.send Sending " + params.method + " request to URL " + baseURL + params.path);
+	App.LOG("App.API.send Sending payload " + JSON.stringify(params.data));
+
+	var xhr = createHTTPClient(params)
+	
+	var url = (baseURL + params.path);
+	
+	try {
+
+		xhr.open(params.method, url);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.send(params.method == "POST" ? (params.data ? params.data : null) : null);
+
+	} catch(e) {
+		Ti.UI.createAlertDialog({
+			title : "Server Error",
+			message : "The server at the given URL did not respond"
+		}).show();
+	}
+
+}; 
+
+exports.initialize = function(app){
+	App = app; 
+}
